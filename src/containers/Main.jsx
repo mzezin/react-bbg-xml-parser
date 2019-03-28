0c18d59c62890dacbfcd780e988da847ac61bf23
Maksim Zezin <mzezin@gmail.com>
2019-03-28 16:21:40 +0300

    clear data button

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -7,12 +7,17 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import parseXml from '../utils';
 
 const Main = () => {
+  const [xml, setXml] = useState('');
   const [json, setJson] = useState('');
   return (
     <Container style={{ margin: 20, padding: 20 }} textAlign="center">
       <Form>
         <TextArea
-          onChange={(e, { value }) => setJson(parseXml(value))}
+          value={xml}
+          onChange={(e, { value }) => {
+            setXml(value);
+            setJson(parseXml(value));
+          }}
           style={{ resize: 'none' }}
           rows={5}
           placeholder="Enter xml data..."
@@ -26,6 +31,15 @@ const Main = () => {
         <CopyToClipboard text={json}>
           <Button style={{ margin: 10, padding: 10 }}>Copy to clipboard</Button>
         </CopyToClipboard>
+        <Button
+          style={{ margin: 10, padding: 10 }}
+          onClick={() => {
+            setXml('');
+            setJson('');
+          }}
+        >
+          Clear Data
+        </Button>
       </Form>
     </Container>
   );
